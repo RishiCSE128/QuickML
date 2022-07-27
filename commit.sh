15d5de9481789125b56d01ba14072229183b9bbb
Vladi756 <kosticv@lsbu.ac.uk>
2022-07-27 17:33:40 +0100

    adds the table

--- commit.sh
+++ commit.sh
@@ -1,4 +1,5 @@
 msg=$1
+echo $msg
 git add .
-git commit -m $msg
+git commit -m "$msg"
 git push
--- webapp/views.py
+++ webapp/views.py
@@ -1,21 +1,22 @@
+import json
 from flask import Blueprint, render_template, request, redirect, url_for
 import pandas as pd
 from sourceCode import Data_Pre_Processing
 
-views = Blueprint('home', __name__)
+views = Blueprint('base', __name__)
 
 @views.route('/')
-def home(): 
+def base(): 
     return render_template('base.html')
+
 
 @views.route('/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
     dataSet = pd.read_csv(uploaded_file)
 
-    print(dataSet.columns)
 
-    return redirect(url_for('home.home'))
+    return render_template('home.html', attributes = list(dataSet.columns))
     # if uploaded_file.filename != '':
     #     uploaded_file.save(uploaded_file.filename)
     # 
