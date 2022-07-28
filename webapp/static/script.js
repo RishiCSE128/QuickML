cd06b9ad89e3753cbc3f2b496f4a238e35aa33d7
Vladi756 <kosticv@lsbu.ac.uk>
2022-07-28 20:46:21 +0100

    Improved UI

--- webapp/static/script.js
+++ webapp/static/script.js
@@ -2,7 +2,7 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
@@ -19,7 +19,29 @@ function showDiv() {
 
 document.querySelectorAll('.choice').forEach(item => {
   item.addEventListener("click", event => {
-      var value = item.value;
-      console.log(value);
-    })
+    var value = item.value;
+    console.log(value);
+  })
 })
+
+function makeVarMap1() {
+  var tbl = $('table#data tr').map(function() {
+    return $(this).find('td').map(function() {
+      return $(this).text();
+    }).get();
+  }).get();
+  console.log(tbl)
+}
+
+
+
+//document.getElementById("someId").getElementsByClassName("someClass")[0].getElementsByTagName("div")[0]
+
+
+
+// function makeVarMap() {
+//   [...document.getElementsByClassName(".dynamic")].forEach(item => {
+//     var key = $("input[type='radio']:checked").val();
+//     console.log(key)
+//   })
+// }
