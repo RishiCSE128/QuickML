050826c63d6e69f86acc7a7bce14b35eba86897c
Vladi756 <kosticv@lsbu.ac.uk>
2022-08-11 19:33:30 +0100

    Transposing

--- sourceCode/Simple_Linear_Regression.py
+++ sourceCode/Simple_Linear_Regression.py
@@ -11,17 +11,26 @@ def simpleLinearRegression(Xtest, Xtrain, Ytest, Ytrain, dataset='Salary data'):
     
     regressor = LinearRegression()
     regressor.fit(Xtrain, Ytrain)   
-    
+
     # print(Xtest[1:,1:].flatten())
     # print(Ytest[1:,1:].flatten())
     # print(Ytrain[1:,1:].flatten())
     # print(Xtrain[1:,1:].flatten())
 
     # plt.suptitle(f'Linear Regression on {dataset}')
-    plt.scatter(Xtest[1:, 1:].flatten(), Ytest[1:, 1:].flatten(), color='blue', label='Test Samples')
-    plt.scatter(Xtrain[1:, 1:].flatten(),Ytrain[1:, 1:].flatten(),color='red', label = 'Train Samples')
+
+
+
+    plt.scatter(Xtest[1:, 1:].flatten(), 
+                Ytest[1:, 1:].flatten(), 
+                color='blue', label='Test Samples')
+    
+    plt.scatter(Xtrain[1:, 1:].flatten(),
+                Ytrain[1:, 1:].flatten(),
+                color='red', 
+                label = 'Train Samples')
   
-    plt.plot(Xtest[1:, 1:].flatten(), regressor.predict(Xtest)[1:, 1:].flatten(), 'g:', label='Regression line')
+    plt.plot(Xtest[1:, 1:].transpose(), regressor.predict(Xtest)[1:, 1:].flatten(), 'g:', label='Regression line')
 
     plt.legend()
     plt.grid()
