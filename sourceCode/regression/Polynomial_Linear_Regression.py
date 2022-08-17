dd71a198b362d7b736ff21bc2ddb3c530bb90400
Vladi756 <kosticv@lsbu.ac.uk>
2022-08-17 19:02:08 +0100

    Polynomial Linear Regression API Implementation

---
+++ sourceCode/regression/Polynomial_Linear_Regression.py
@@ -0,0 +1,24 @@
+from cProfile import label
+from email.encoders import encode_7or8bit
+from turtle import color
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+
+import random
+
+# Using TgAgg backend to make matplotlib thread safe
+mpl.use("TkAgg")
+
+# import statsmodels.formula.api 
+
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import confusion_matrix
+
+# POLYNOMIAL LINEAR REGRESSION 
+def polynomialLinearRegression(Xtest, Xtrain, Ytest, Ytrain, dataSet):
+
+    # TO DO 
+
+    return 1
